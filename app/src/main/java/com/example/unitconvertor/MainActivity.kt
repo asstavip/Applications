17d080b346f17f136bd35dc06aa7effe30c3c8ab
abderrahman <Abdo.bissari@gmail.com>
2024-10-04 19:22:09 +0100

    making interactive input fields and dropDown menus with Compose

--- app/src/main/java/com/example/unitconvertor/MainActivity.kt
+++ app/src/main/java/com/example/unitconvertor/MainActivity.kt
@@ -1,30 +1,45 @@
 package com.example.unitconvertor
 
 import android.animation.TypeConverter
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.unitconvertor.ui.theme.UnitConvertorTheme
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -32,9 +47,8 @@ class MainActivity : ComponentActivity() {
             UnitConvertorTheme {
                 Scaffold(
                     modifier = Modifier.fillMaxSize())
-                { innerPadding ->
-                    unitConverter(
-                        modifier = Modifier.padding(innerPadding)
+                {
+                    UnitConverter(
                     )
                 }
             }
@@ -43,30 +57,121 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun unitConverter(modifier: Modifier = Modifier) {
+fun UnitConverter() {
+    var inputValue by remember { mutableStateOf("") }
+    var outputValue by remember { mutableStateOf("") }
+    var inputUnit by remember { mutableStateOf("Centimeters") }
+    var outputUnit by remember { mutableStateOf("Meters") }
+    var iExpanded by remember { mutableStateOf(false) }
+    var oExpanded by remember { mutableStateOf(false) }
+    var convertor by remember { mutableStateOf(0.01) }
 
-    Column(modifier = modifier) {
+    Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment = CenterHorizontally) {
         Text("Unit Converter")
-        OutlinedTextField(value = "", onValueChange = {})
-        Row {
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        OutlinedTextField(value = inputValue, onValueChange = {
+            inputValue = it
+        }, label = { Text("Input Value") }, placeholder = { Text("Ex: 10") })
+
+        Row (modifier = Modifier.padding(10.dp), horizontalArrangement = Arrangement.spacedBy(50.dp)) {
             Box {
-                Button(onClick = {/**/}) {
+                Button(onClick = {
+                    iExpanded = !iExpanded
+                }) {
                     Text("Select")
                     Icon(Icons.Default.ArrowDropDown, contentDescription = "Arrow Drop Down")
                 }
+                DropdownMenu(expanded = iExpanded, onDismissRequest = {
+                    iExpanded = !iExpanded
+                }) {
+                    DropdownMenuItem(
+                        text = { Text("Centimeters") },
+                        onClick = {
+                            iExpanded = !iExpanded
+                            inputUnit = "Centimeters"
+                            convertor = 0.01
+                        }
+                    )
+                    DropdownMenuItem(
+                        text = { Text("Meters") },
+                        onClick = {
+                            /**/
+                        }
+                    )
+                    DropdownMenuItem(
+                        text = { Text("Kilometers") },
+                        onClick = {
+                            /**/
+                        }
+                    )
+                    DropdownMenuItem(
+                        text = { Text("Feet") },
+                        onClick = {
+                            /**/
+                        }
+                    )
+                    DropdownMenuItem(
+                        text = { Text("Inches") },
+                        onClick = {
+                            /**/
+                        }
+                    )
+
+                }
             }
             Box {
-                Button(onClick = {/**/}) {
+                Button(onClick = {
+                    oExpanded = !oExpanded}) {
                     Text("Select")
                     Icon(Icons.Default.ArrowDropDown, contentDescription = "Arrow Drop Down")
                 }
+                DropdownMenu(expanded = oExpanded, onDismissRequest = {
+                    oExpanded = !oExpanded
+                }) {
+                    DropdownMenuItem(
+                        text = { Text("Centimeters") },
+                        onClick = {
+                            oExpanded = !oExpanded
+                            outputUnit = "Centimeters"
+                            convertor = 0.01
+                        }
+                    )
+                    DropdownMenuItem(
+                        text = { Text("Meters") },
+                        onClick = {
+                            /**/
+                        }
+                    )
+                    DropdownMenuItem(
+                        text = { Text("Kilometers") },
+                        onClick = {
+                            /**/
+                        }
+                    )
+                    DropdownMenuItem(
+                        text = { Text("Feet") },
+                        onClick = {
+                            /**/
+                        }
+                    )
+                    DropdownMenuItem(
+                        text = { Text("Inches") },
+                        onClick = {
+                            /**/
+                        }
+                    )
+
+                }
             }
-        }
+            }
         Row {
             Text("Result:")
         }
+        }
+
     }
-}
 
 
 @Composable
@@ -80,5 +185,5 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground =true, showSystemUi = true)
 @Composable
 fun unitConverterPreview(){
-    unitConverter()
+    UnitConverter()
 }
